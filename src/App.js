bf5bb69d9ca18cf174756adf439a5c207603acd2
Sarthak Sen <sarthak.sen.01@gmail.com>
2021-07-04 01:58:48 +0530

    Navbar and Page Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is Home Page
-      </Route>
-      <Route exact path="/starred">
-        This is Starred Page
-      </Route>
-      <Route>This is 404</Route>
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>This is 404</Route>
+      </Switch>
+    </div>
   );
 }
 
