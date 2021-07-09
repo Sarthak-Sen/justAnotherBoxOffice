1daafd731123ae7ee78c99a5bad3659331f14793
Sarthak Sen <sarthak.sen.01@gmail.com>
2021-07-09 19:56:50 +0530

    styled the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import Show from './pages/Show';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/starred">
-        <Starred />
-      </Route>
-      <Route exact path="/show/:id">
-        <Show />
-      </Route>
-      <Route>This is 404</Route>
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
+        <Route>This is 404</Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
