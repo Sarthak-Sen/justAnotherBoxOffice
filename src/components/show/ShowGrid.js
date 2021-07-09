3c37144453d2b962c47d74b89291fdbd6627a018
Sarthak Sen <sarthak.sen.01@gmail.com>
2021-07-10 02:13:03 +0530

    optimized missed

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,5 +1,6 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useCallback } from 'react';
 import ShowCard from './ShowCard';
 import { FlexGrid } from '../styled';
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
@@ -12,13 +13,15 @@ const ShowGrid = ({ data }) => {
     <FlexGrid>
       {data.map(({ show }) => {
         const isStarred = starredShows.includes(show.id);
-        const onStarClick = () => {
+
+        const onStarClick = useCallback(() => {
           if (isStarred) {
             dispatchStarred({ type: 'REMOVE', showId: show.id });
           } else {
             dispatchStarred({ type: 'ADD', showId: show.id });
           }
-        };
+        }, [isStarred, show.id]);
+
         return (
           <ShowCard
             key={show.id}
